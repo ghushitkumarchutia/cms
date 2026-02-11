108f6ec2f8682a260ee12c4b7233ec2fd7d4ceb1
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-02-11 14:26:35 +0530

    feat(cms-backend): Update artifact schema model with like and commets

--- cms-backend/src/models/artifact.model.js
+++ cms-backend/src/models/artifact.model.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
 const artifactSchema = new mongoose.Schema(
   {
     title: String,
     description: String,
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [commentSchema],
   },
   { timestamps: true },
 );
