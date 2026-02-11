d4442e78a58e545560dee67e419ec6d7acb6d699
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-02-11 14:34:12 +0530

    feat(cms-backend): Update routes

--- cms-backend/src/routes/artifact.routes.js
+++ cms-backend/src/routes/artifact.routes.js
@@ -5,4 +5,10 @@ const auth = require("../middlewares/auth.middleware");
 router.post("/", auth, ctrl.createArtifact);
 router.get("/", auth, ctrl.getArtifacts);
 
+router.post("/:id/like", auth, ctrl.toggleLike);
+router.get("/:id/likes", auth, ctrl.getLikes);
+
+router.post("/:id/comment", auth, ctrl.addComment);
+router.get("/:id/comments", auth, ctrl.getComments);
+
 module.exports = router;
