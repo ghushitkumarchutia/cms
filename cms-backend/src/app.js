874eeed28da04aeb7ab7f590659d99285929fea5
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-02-11 15:54:01 +0530

    Add rate limit

--- cms-backend/src/app.js
+++ cms-backend/src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimiter = require("./middlewares/rateLimiter");
 const morgan = require("morgan");
 
 const app = express();
-
+app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
+app.use(express.json({ limit: "10mb" }));
+
+app.use("/api", rateLimiter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the CMS Backend API");
